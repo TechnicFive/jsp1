11e6366588f1d7936c74dec58db0babfb6be9571
TechnicFive <flufi@usal.es>
2021-10-28 21:46:43 +0200

    Insertar roles

--- src/main/java/DAO/RolDAO.java
+++ src/main/java/DAO/RolDAO.java
@@ -3,11 +3,23 @@ package DAO;
 import java.util.List;
 
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 import Hibernate.HibernateUtil;
 import tablas.Roles;
 
 public class RolDAO {
+	static Transaction tx = null;
+	public static void insertRol(String nombre) {
+		insertRol(HibernateUtil.retrieveSession(), nombre);
+	}
+	public static void insertRol(Session s, String nombre) {
+		tx = s.beginTransaction();
+		//public Empleado(int codigo, String nombre, String apellido1, String apellido2, String lugarNacimiento, String fechaNacimiento, String direccion, String telefono, String puesto, int codDepartamento)
+		Roles client = new Roles(nombre);
+		s.save(client);
+		tx.commit();
+	}
 	
 	public static List<Roles> getAllRoles() {
 		return getAllRoles(HibernateUtil.retrieveSession());
